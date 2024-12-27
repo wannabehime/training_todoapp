ccebbddc01919c5de040176eaee6417cd36878ce
wannabehime <snsnap1174@SnSnap1174noMacBook-Pro.local>
2024-12-27 18:27:39 +0900

    Installed Node

---
+++ tmp_src/components/TodoForm.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+const greeting = ref('Hello World!')
+</script>
+
+<template>
+    <h1>TODO LIST</h1>
+    <input v-model="newTodo" placeholder="Write your todo list here..." />
+    <button @click="addTodo">ADD</button>
+</template>
+
+<style>
+.greeting {
+  color: red;
+  font-weight: bold;
+}
+</style>
